4e4807e3c99d210f5978199c22d53256bf6aaa33
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-10-09 16:30:34 +1100

    Added power-management stuff for laptop

--- configuration.nix
+++ configuration.nix
@@ -86,8 +86,28 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Prevents overheating and works well with intel CPUs;
-  services.thermald.enable = true;
+  # Power management adjustments for laptop:
+  services = {
+    # Better scheduling for CPU cycles:
+    system76-scheduler.settings.cfsProfiles.enable = true;
+    # Prevents overheating and works well with intel CPUs;
+    thermald.enable = true;
+
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_BAT = 0;
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      };
+    };
+
+    # Disable GNOMEs power management
+    power-profiles-daemon.enable = false;
+  };
+  # Enable powertop
+  powerManagement.powertop.enable = true;
 
   home-manager = {
     extraSpecialArgs = { inherit inputs; };
