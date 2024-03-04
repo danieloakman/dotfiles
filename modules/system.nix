f8197a35c0eff9ef1adaf930f336f8a661691684
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-03-05 08:23:07 +1100

    Added a comment

--- modules/system.nix
+++ modules/system.nix
@@ -240,6 +240,7 @@
       vlc # For video playback
       gimp
       # ventoy # For creating bootable USBs. It's really cool, just drag and drop ISOs onto the USB and you can select which one to boot from
+      # foot # Maybe can use this for quick to load terminal that's a replacement for dmenu in gnome wayland
     ];
   };
 
