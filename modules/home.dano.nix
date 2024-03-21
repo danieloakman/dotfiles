72658f06623ee2e2de39d2c55413939cff1ecd38
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-03-22 05:39:56 +1100

    Added nixos-search alias

--- modules/home.dano.nix
+++ modules/home.dano.nix
@@ -270,6 +270,7 @@
         nixos-build = "sudo nixos-rebuild build --flake ~/repos/personal/nixos/#$HOST";
         nixos-gc = "sudo nix-collect-garbage --delete-older-than 15d";
         nixos-update = "sudo nix flake update ~/repos/personal/nixos";
+        nixos-search = "nix search nixpkgs";
       };
       oh-my-zsh = {
         enable = true;
