41df6dd0fabf9d9c6356590e6304445505e4bcdb
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-03-25 12:14:14 +1100

    Fixed outdated option

--- modules/home.dano.nix
+++ modules/home.dano.nix
@@ -251,7 +251,7 @@
 
     zsh = {
       enable = true;
-      enableAutosuggestions = true;
+      autosuggestion.enable = true;
       enableCompletion = true;
       syntaxHighlighting.enable = true;
       initExtra = ''
