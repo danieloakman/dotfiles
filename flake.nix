803bd26a491341786cd465fa662406ec142d961d
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-04-02 09:45:59 +1100

    Commented out this nix cache part, for now
    as it might be causing long build times

--- flake.nix
+++ flake.nix
@@ -25,21 +25,21 @@
       #   };
       # };
       createNixCache = ({ ... }: {
-        nix = {
-          registry = {
-            nixpkgs.flake = nixpkgs;
-          };
-          settings = {
-            substituters = [
-              "https://nix-community.cachix.org"
-              "https://srid.cachix.org"
-            ];
-            trusted-public-keys = [
-              "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-              "srid.cachix.org-1:MTQ6ksbfz3LBMmjyPh0PLmos+1x+CdtJxA/J2W+PQxI="
-            ];
-          };
-        };
+        # nix = {
+        #   registry = {
+        #     nixpkgs.flake = nixpkgs;
+        #   };
+        #   settings = {
+        #     substituters = [
+        #       "https://nix-community.cachix.org"
+        #       "https://srid.cachix.org"
+        #     ];
+        #     trusted-public-keys = [
+        #       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        #       "srid.cachix.org-1:MTQ6ksbfz3LBMmjyPh0PLmos+1x+CdtJxA/J2W+PQxI="
+        #     ];
+        #   };
+        # };
       });
     in
     {
