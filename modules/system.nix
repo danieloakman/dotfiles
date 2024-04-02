d6892696148589892935c7921ee7ba67048ddba1
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-04-02 12:12:32 +1100

    Removed dropbox

--- modules/system.nix
+++ modules/system.nix
@@ -177,7 +177,7 @@
       vlc # For video playback
       gimp
       obsidian
-      dropbox
+      # dropbox # Was trying this out for syncing with mobile. But offline sync on mobile is only available for paid users.
       # ventoy # For creating bootable USBs. It's really cool, just drag and drop ISOs onto the USB and you can select which one to boot from
       # foot # Maybe can use this for quick to load terminal that's a replacement for dmenu in gnome wayland
     ] ++ (if env.isOnWayland then [
