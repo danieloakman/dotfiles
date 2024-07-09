e4eea25da12ae268520479eb2e7b37dc29ff33c0
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-07-10 08:54:01 +1000

    Some basic keybinds

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -7,11 +7,12 @@ let
     ${pkgs.waybar}/bin/waybar &
     ${pkgs.swww}/bin/swww init &
     ${pkgs.dunst}/bin/dunst &
-    ${pkgs.rofi-wayland}/bin/rofi -show drun -show-icons &
     ${pkgs.kitty}/bin/kitty &
 
     sleep 1
   '';
+  # ${pkgs.rofi-wayland}/bin/rofi -show drun -show-icons &
+  # ${pkgs.guake}/bin/guake &
   # ${pkgs.swww}/bin/swww img ${./wallpaper.png} &
   hyprPkgs = inputs.hyprland.packages."${pkgs.system}";
   hyprPlugins = inputs.hyprland-plugins.packages."${pkgs.system}";
@@ -19,23 +20,28 @@ in
 {
   # Enable cachix for hyprland, otherwise hyprland will be built from source:
   nix.settings = {
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    substituters = [ "https://hyprland.cachix.org" ];
+    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
   };
 
-  environment.systemPackages = with pkgs; [
-    # pyprland
-    hyprpicker
-    hyprcursor
-    hyprlock
-    hypridle
-    # hyprpaper
-    kitty
-    rofi-wayland
-    waybar
-    dunst
-    swww
-  ];
+  environment = {
+    sessionVariables = {
+      GUAKE_ENABLE_WAYLAND = 1;
+    };
+    systemPackages = with pkgs; [
+      # pyprland
+      hyprpicker
+      hyprcursor
+      hyprlock
+      hypridle
+      # hyprpaper
+      kitty
+      rofi-wayland
+      waybar
+      dunst
+      swww
+    ];
+  };
 
   programs = {
     hyprland = {
@@ -68,9 +74,34 @@ in
         exec-once = ''${startupScript}/bin/start'';
 
         "$mod" = "SUPER";
+        bind = [
+          "$mod, F, exec, rofi -show drun -show-icons"
+          "$mod, Q, exec, kitty"
+          # "$mod, Q, exec, guake-toggle"
+        ]
+        ++ (
+          # workspaces
+          # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+          builtins.concatLists (builtins.genList
+            (
+              x:
+              let
+                ws =
+                  let
+                    c = (x + 1) / 10;
+                  in
+                  builtins.toString (x + 1 - (c * 10));
+              in
+              [
+                "$mod, ${ws}, workspace, ${toString (x + 1)}"
+                "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+              ]
+            )
+            10)
+        );
 
         # TODO: This monitor is specific to the tiny laptop, will need to move this to that specific config.
-        monitor="eDP-1, 1920x1200, 0x0, 1.0";
+        monitor = "eDP-1, 1920x1200, 0x0, 1.0";
 
         input = {
           natural_scroll = true;
@@ -79,20 +110,20 @@ in
           };
         };
 
-        "plugin:borders-plus-plus" = {
-          add_borders = 1; # 0 - 9
+        # "plugin:borders-plus-plus" = {
+        #   add_borders = 1; # 0 - 9
 
-          # you can add up to 9 borders
-          "col.border_1" = "rgb(ffffff)";
-          "col.border_2" = "rgb(2222ff)";
+        #   # you can add up to 9 borders
+        #   "col.border_1" = "rgb(ffffff)";
+        #   "col.border_2" = "rgb(2222ff)";
 
-          # -1 means "default" as in the one defined in general:border_size
-          border_size_1 = 10;
-          border_size_2 = -1;
+        #   # -1 means "default" as in the one defined in general:border_size
+        #   border_size_1 = 10;
+        #   border_size_2 = -1;
 
-          # makes outer edges match rounding of the parent. Turn on / off to better understand. Default = on.
-          natural_rounding = "yes";
-        };
+        #   # makes outer edges match rounding of the parent. Turn on / off to better understand. Default = on.
+        #   natural_rounding = "yes";
+        # };
       };
     };
   };
