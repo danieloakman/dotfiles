282b539973b4883794eb17439a0e4df62047a540
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-07-10 13:14:56 +1000

    Added gamemode hotkey super+f10

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -14,6 +14,21 @@ let
   # ${pkgs.rofi-wayland}/bin/rofi -show drun -show-icons &
   # ${pkgs.guake}/bin/guake &
   # ${pkgs.swww}/bin/swww img ${./wallpaper.png} &
+  gamemodeScript = pkgs.pkgs.writeShellScriptBin "start" ''
+    HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
+    if [ "$HYPRGAMEMODE" = 1 ] ; then
+        hyprctl --batch "\
+            keyword animations:enabled 0;\
+            keyword decoration:drop_shadow 0;\
+            keyword decoration:blur:enabled 0;\
+            keyword general:gaps_in 0;\
+            keyword general:gaps_out 0;\
+            keyword general:border_size 1;\
+            keyword decoration:rounding 0"
+        exit
+    fi
+    hyprctl reload
+  '';
   hyprPkgs = inputs.hyprland.packages."${pkgs.system}";
   hyprPlugins = inputs.hyprland-plugins.packages."${pkgs.system}";
 in
@@ -80,6 +95,7 @@ in
         bind = [
           "$mod, S, exec, rofi -show drun -show-icons"
           "$mod, Q, exec, kitty"
+          "$mod, F10, exec, ${gamemodeScript}/bin/start"
           # "$mod, Q, exec, guake-toggle"
         ]
         ++ (
