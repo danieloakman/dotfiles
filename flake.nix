0528b7b75e538b4f70abeaabc1103f4d6f1750c6
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-07-12 06:34:35 +1000

    Feature/stylix (#5)
    * Added a comment
    
    * Added stylix and a starfield image to base the colour scheme off of
    
    As well as turning the nix-cache fn back on by uncommenting it
    
    * Updated flake since we added stylix
    
    * Removed unused `pkgs` var
    
    * Created a commonImports fn in flake.nix and moved some imports there
    
    * Fixed typo
    
    * Attempted to fix a bug
    
    * More stylix options

--- flake.nix
+++ flake.nix
@@ -12,9 +12,10 @@
     };
     devenv.url = "github:cachix/devenv";
     openvpn24.url = "github:nixos/nixpkgs/2d38b664b4400335086a713a0036aafaa002c003";
+    stylix.url = "github:danth/stylix";
   };
 
-  outputs = { self, nixpkgs, devenv,... }@inputs:
+  outputs = { self, nixpkgs, devenv, ... }@inputs:
     let
       system = "x86_64-linux";
       # pkgs = import nixpkgs {
@@ -25,21 +26,32 @@
       #   };
       # };
       createNixCache = ({ ... }: {
-        # nix = {
-        #   registry = {
-        #     nixpkgs.flake = nixpkgs;
-        #   };
-        #   settings = {
-        #     substituters = [
-        #       "https://nix-community.cachix.org"
-        #       "https://srid.cachix.org"
-        #     ];
-        #     trusted-public-keys = [
-        #       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        #       "srid.cachix.org-1:MTQ6ksbfz3LBMmjyPh0PLmos+1x+CdtJxA/J2W+PQxI="
-        #     ];
-        #   };
-        # };
+        nix = {
+          registry = {
+            nixpkgs.flake = nixpkgs;
+          };
+          settings = {
+            substituters = [
+              "https://nix-community.cachix.org"
+              "https://srid.cachix.org"
+            ];
+            trusted-public-keys = [
+              "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+              "srid.cachix.org-1:MTQ6ksbfz3LBMmjyPh0PLmos+1x+CdtJxA/J2W+PQxI="
+            ];
+          };
+        };
+      });
+      commonImports = ({ ... }: {
+        # Imports inherit inputs system; used across all host configurations:
+        imports = [
+          inputs.home-manager.nixosModules.home-manager
+          inputs.stylix.nixosModules.stylix
+          # This requires env, which is currently defined in the host/configuration.nix, so it can't be imported here (for now).
+          # (import ./modules/system.nix { inherit lib inputs config pkgs env; })
+          ./modules/stylix.nix
+          ./modules/user.nix
+        ];
       });
     in
     {
@@ -48,21 +60,30 @@
           specialArgs = { inherit inputs system; };
           modules = [
             ./hosts/djo-personal-desktop/configuration.nix
-            createNixCache {}
+            commonImports
+            { }
+            createNixCache
+            { }
           ];
         };
         djo-personal-laptop = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs system; };
           modules = [
             ./hosts/djo-personal-laptop/configuration.nix
-            createNixCache {}
+            commonImports
+            { }
+            createNixCache
+            { }
           ];
         };
         djo-tiny-laptop = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs system; };
           modules = [
             ./hosts/djo-tiny-laptop/configuration.nix
-            createNixCache {}
+            commonImports
+            { }
+            createNixCache
+            { }
           ];
         };
       };
--- hosts/djo-personal-desktop/configuration.nix
+++ hosts/djo-personal-desktop/configuration.nix
@@ -13,10 +13,8 @@ in
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    inputs.home-manager.nixosModules.home-manager
     (import ../../modules/system.nix { inherit lib inputs config pkgs env; })
     ../../modules/desktop-pkgs.nix
-    ../../modules/user.nix
     ../../modules/gnome.nix
     (import ../../modules/power-management.nix { inherit env; })
     ../../modules/mobile-dev.nix
