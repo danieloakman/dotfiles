2e6e163ff23ccc0d47b9692874b713d036c7af22
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-07-12 16:57:12 +1000

    More hyprland config

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -123,15 +123,99 @@ in
             10)
         );
 
+        binds = {
+          workspace_back_and_forth = true;
+          allow_workspace_cycles = true;
+          pass_mouse_when_bound = false;
+        };
+
         monitor = env.hyprland.monitor;
 
         input = {
           natural_scroll = true;
+
+          kb_layout = "us";
+          # kb_variant =
+          # kb_model =
+          # kb_options =
+          # kb_rules =
+          repeat_rate = 50;
+          repeat_delay = 300;
+          numlock_by_default = true;
+          left_handed = false;
+          follow_mouse = true;
+          float_switch_override_focus = false;
+
           touchpad = {
-            natural_scroll = true;
+            disable_while_typing = true;
+            natural_scroll = true; # Was false from dots' config
+            clickfinger_behavior = false;
+            middle_button_emulation = true;
+            tap-to-click = true;
+            drag_lock = false;
+          };
+
+          # below for devices with touchdevice ie. touchscreen
+          # TODO: set up env for touch device
+          # touchdevice = {
+          #   enabled = true;
+          # };
+
+          # below is for table see link above for proper variables
+          # TODO: Set up env for tablet 
+          # tablet = {
+          #   transform = 0;
+          #   left_handed = 0;
+          # };
+        };
+
+        gestures = {
+          workspace_swipe = true;
+          workspace_swipe_fingers = 3;
+          workspace_swipe_distance = 500;
+          workspace_swipe_invert = true;
+          workspace_swipe_min_speed_to_force = 30;
+          workspace_swipe_cancel_ratio = 0.5;
+          workspace_swipe_create_new = true;
+          workspace_swipe_forever = true;
+          #workspace_swipe_use_r = true #uncomment if wanted a forever create a new workspace with swipe right
+        };
+
+        # Could help when scaling and not pixelating
+        xwayland = {
+          force_zero_scaling = true;
+        };
+
+        # cursor section for Hyprland >= v0.41.0
+        # cursor = {
+        #   no_hardware_cursors = false;
+        #   enable_hyprcursor = true;
+        #   warp_on_change_workspace = true; # for -git or Hyprland >v0.41.1
+        # };
+
+        group = {
+          col.border_active = "$color15";
+
+          groupbar = {
+            col.active = "$color0";
           };
         };
 
+        misc = {
+          disable_hyprland_logo = true;
+          disable_splash_rendering = true;
+          middle_click_paste = false;
+
+          # Don't know what these do:
+          vfr = true;
+          #vrr = 0
+          mouse_move_enables_dpms = true;
+          enable_swallow = true;
+          swallow_regex = "^(kitty)$";
+          focus_on_activate = false;
+          no_direct_scanout = true; # for fullscreen games
+          initial_workspace_tracking = 0;
+        };
         # "plugin:borders-plus-plus" = {
         #   add_borders = 1; # 0 - 9
 
