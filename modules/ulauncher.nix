28cc052ed68c8650b6248caf53c4c671f0ab37d4
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-08-05 10:28:55 +1000

    Updated ulauncher systemd service

--- modules/ulauncher.nix
+++ modules/ulauncher.nix
@@ -25,7 +25,7 @@ in
     script = "${pkgs.ulauncher}/bin/ulauncher --hide-window";
 
     documentation = [ "https://github.com/Ulauncher/Ulauncher/blob/f0905b9a9cabb342f9c29d0e9efd3ba4d0fa456e/contrib/systemd/ulauncher.service" ];
-    wantedBy = [ "graphical.target" "multi-user.target" ];
-    after = [ "display-manager.service" ];
+    wantedBy = [ "graphical-session.target" "default.target" ];
+    after = [ "gnome-session-monitor.service" ];
   };
 }
