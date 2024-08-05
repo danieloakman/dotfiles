07347dfd8210ceb53c0777eb3d5b477acf164085
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-08-05 10:52:15 +1000

    Added some comments

--- modules/ulauncher.nix
+++ modules/ulauncher.nix
@@ -4,6 +4,7 @@ let
     (oldAttrs: {
       propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ (with pkgs; [
         python3
+        # These are needed for our ulauncher plugins
         python3Packages.pip
         python3Packages.requests
         python3Packages.pint
@@ -25,7 +26,9 @@ in
     script = "${pkgs.ulauncher}/bin/ulauncher --hide-window";
 
     documentation = [ "https://github.com/Ulauncher/Ulauncher/blob/f0905b9a9cabb342f9c29d0e9efd3ba4d0fa456e/contrib/systemd/ulauncher.service" ];
+    # `systemctl --user --type target,service` to see what's available
     wantedBy = [ "graphical-session.target" "default.target" ];
+    # TODO: probably should move ulauncher.nix to be a gnome dependent module
     after = [ "gnome-session-monitor.service" ];
   };
 }
