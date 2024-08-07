e867993c328667d97e4f9b37206d2f8aefb48f7f
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-08-07 19:41:37 +1000

    Added ulauncher settings to home-manager

--- modules/ulauncher.nix
+++ modules/ulauncher.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, env, ... }:
 let
   ulauncher = pkgs.ulauncher.overrideAttrs
     (oldAttrs: {
@@ -32,5 +32,21 @@ in
     after = [ "gnome-session-monitor.service" ];
   };
 
-  # TODO: move ~/.config/ulauncher files to home-manager
+  home-manager.users.${env.user}.home = {
+    # "hotkey-show-app" has been unset because <Super>space doesn't seem to work. So we're just setting the hotkey in gnome (imperatively) instead.
+    file.".config/ulauncher/settings.json".text = ''
+      {
+        "hotkey-show-app": "",
+        "blacklisted-desktop-dirs": "/usr/share/locale:/usr/share/app-install:/usr/share/kservices5:/usr/share/fk5:/usr/share/kservicetypes5:/usr/share/applications/screensavers:/usr/share/kde4:/usr/share/mimelnk",
+        "clear-previous-query": true,
+        "disable-desktop-filters": false,
+        "grab-mouse-pointer": false,
+        "render-on-screen": "mouse-pointer-monitor",
+        "show-indicator-icon": true,
+        "show-recent-apps": "0",
+        "terminal-command": "",
+        "theme-name": "dark"
+      }
+    '';
+  };
 }
