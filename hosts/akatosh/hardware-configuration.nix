ae3b67a6d44395e2c9dde715a970df7ce5ba1c2c
Daniel Brown <42539848+danieloakman@users.noreply.github.com>
2025-02-03 12:27:29 +1100

    Move docker hardware nvidia related option to docker module

--- hosts/akatosh/hardware-configuration.nix
+++ hosts/akatosh/hardware-configuration.nix
@@ -39,7 +39,4 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-
-  # Enable for GPU pass-through support on things like Docker conainters:
-  hardware.nvidia-container-toolkit.enable = true;
 }
--- modules/docker.nix
+++ modules/docker.nix
@@ -1,4 +1,6 @@
 { env, ... }:{
   virtualisation.docker.enable = true;
   users.users.${env.user}.extraGroups = [ "docker" ];
-}
+
+  # Enable for GPU pass-through support on things like Docker conainters:
+  hardware.nvidia-container-toolkit.enable = true;
