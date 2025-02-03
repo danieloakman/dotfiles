07a58300843b03c20e337b8e370c2c9148b3e28c
Daniel Brown <42539848+danieloakman@users.noreply.github.com>
2025-02-03 12:27:34 +1100

    More docker pkgs

--- modules/docker.nix
+++ modules/docker.nix
@@ -1,6 +1,17 @@
-{ env, ... }:{
-  virtualisation.docker.enable = true;
+{ env, pkgs, ... }: {
+  virtualisation.docker = {
+    enable = true;
+    enableOnBoot = true;
+  };
   users.users.${env.user}.extraGroups = [ "docker" ];
 
   # Enable for GPU pass-through support on things like Docker conainters:
   hardware.nvidia-container-toolkit.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    docker
+    docker-compose
+    docker-init
+    lazydocker
+  ];
+}
