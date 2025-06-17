34041df043978e039e796c9aa26309953deeb106
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-06-18 04:16:21 +1000

    Set up easier way to declare keybinds

--- modules/gnome/keybinds.nix
+++ modules/gnome/keybinds.nix
@@ -1,29 +1,41 @@
 # This module is used to set the keybinds for the gnome desktop environment.
-{ env, ... }:
+{ lib, env, ... }:
 let
   mediaKeys = "org/gnome/settings-daemon/plugins/media-keys";
   customKeybindings = "${mediaKeys}/custom-keybindings";
-  # keybinds = [
-  #   {
-  #     path = "${customKeybindings}/custom0";
-  #     name = "Emoji Picker";
-  #     command = "rofi -show emoji";
-  #     binding = "<Super>g";
-  #   }
-  # ];
+  # Declare keybinds here.
+  keybinds = [
+    {
+      name = "Emoji Picker";
+      command = "rofi -show emoji";
+      binding = "<Super>g";
+    }
+    {
+      name = "Rofi Google Search";
+      command = "rofi-google-search";
+      binding = "<Super>s";
+    }
+  ];
 in
 {
   home-manager.users.${env.user}.dconf.settings = {
     ${mediaKeys} = {
-      custom-keybindings = [
-        "${customKeybindings}/custom0/"
-      ];
+      # custom-keybindings = [
+      #   "/${customKeybindings}/custom0/"
+      #   "/${customKeybindings}/custom1/"
+      # ];
+      custom-keybindings = lib.imap1 (i: keybind: "/${customKeybindings}/custom${toString i}/") keybinds;
     };
 
-    "${customKeybindings}/custom0" = {
-      name = "Emoji Picker";
-      command = "rofi -show emoji";
-      binding = "<Super>l";
-    };
-  };
+    # "${customKeybindings}/custom0" = {
+    #   name = "Emoji Picker";
+    #   command = "rofi -show emoji";
+    #   binding = "<Super>g";
+    # };
+  } // lib.listToAttrs (lib.imap1
+    (i: keybind: {
+      name = "${customKeybindings}/custom${toString i}";
+      value = keybind;
+    })
+    keybinds);
 }
