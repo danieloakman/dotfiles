f2f22a050ec4a2dcaefa03474f79966ce75c51e8
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-06-26 14:51:39 +1000

    Move wakeonlan enable to it's own module

--- hosts/akatosh/configuration.nix
+++ hosts/akatosh/configuration.nix
@@ -20,6 +20,7 @@
     ../../modules/docker.nix
     ../../modules/rofi.nix
     ../../modules/syncthing.nix
+    ../../modules/wakeonlan.nix
   ];
 
   # Bootloader
@@ -35,18 +36,6 @@
   };
 
   networking.hostName = "akatosh"; # Define your hostname.
-
-  # Wake-on-LAN configuration for ethernet interface
-  networking = {
-    # Allow the wakeonlan discard port to be used.
-    allowedTCPPorts = [ 9 ];
-    interfaces.enp0s31f6 = {
-      useDHCP = true;
-      # Use `ip link show enp0s31f6` to see the mac address for this interface.
-      # Then on another machine, run `wakeonlan <mac address>` to wake this host up from a sleep state.
-      wakeOnLan.enable = true;
-    };
-  };
 
   hardware = {
     enableRedistributableFirmware = true;
