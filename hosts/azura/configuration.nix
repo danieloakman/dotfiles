e4ab6f6fd906f5deb00feb3fd82041fde455c6ea
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-07-16 05:35:38 +1000

    Fix azura local wallpaper path

--- hosts/azura/configuration.nix
+++ hosts/azura/configuration.nix
@@ -32,7 +32,7 @@
   networking.hostName = "azura"; # Define your hostname. `echo $HOST`
 
   # Required config for imported modules:
-  stylix.image = /home/dano/repos/personal/dotfiles/files/assets/azura-wallpaper.jpeg;
+  stylix.image = ../../files/assets/azura-wallpaper.jpeg;
   home-manager.users.${env.user}.wayland.windowManager.hyprland.settings.monitor = [
     "eDP-1, 1366x768, 0x0, 1.0"
   ];
