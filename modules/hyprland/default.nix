b3d908de4f7bcc780f7b201ac4cd11fc6c2d2985
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-07-16 15:33:28 +1000

    Add more keybinds

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -98,6 +98,9 @@ in
           "Print, exec, hyprshot -o ~/Pictures/Screenshots -m region"
           "XF86MonBrightnessUp, exec, brightnessctl set +10%"
           "XF86MonBrightnessDown, exec, brightnessctl set 10%-"
+          "XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+          "XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+          "XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
 
           "SUPER_SHIFT, right, movetoworkspace, +1"
           "SUPER_SHIFT, left, movetoworkspace, -1"
