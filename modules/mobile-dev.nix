7977387446794deffcb25659454f0941c64733ea
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-07-31 03:33:13 +1000

    Add scrcpy to mobile-dev

--- modules/mobile-dev.nix
+++ modules/mobile-dev.nix
@@ -8,6 +8,7 @@
   environment.systemPackages = with pkgs; [
     android-studio
     android-tools
+    scrcpy # For mirroring the screen of your phone to your computer
   ];
   home-manager.users.${env.user}.home = /* { lib, pkgs, env, ... }: */ {
     # For some reason having this as a function doesn't work. Don't fully understand why. It's possible we'd need to move this to be imported by the home manager module, home.dano.nix or something.
