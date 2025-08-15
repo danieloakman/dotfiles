06e8cdeae450d8ddf576690c065688f543e5f135
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-08-16 05:50:52 +1000

    Hide OnScreenKeyboard widget if there's no detected touch device

--- modules/hyprland/ags/widget/OnscreenKeyboard.tsx
+++ modules/hyprland/ags/widget/OnscreenKeyboard.tsx
@@ -1,8 +1,16 @@
 import { execAsync } from 'ags/process';
 import Icon from '../components/Icon';
-import { createState } from 'ags';
+import { createExternal, createState, With } from 'ags';
+import { noop } from '../utils/fn';
 
 const [isOnScreenKeyboardVisible, setIsOnScreenKeyboardVisible] = createState(false);
+
+export const hasTouchScreen = createExternal(false, (set) => {
+  execAsync('zsh -c "ls /sys/class/input/*/name | xargs cat"')
+    .then((res) => set(res.includes('touch')))
+    .catch(() => set(false));
+  return noop;
+});
 
 export const toggleOnScreenKeyboard = () =>
   execAsync(`zsh -c "kill -34 $(ps -C wvkbd-mobintl | awk 'NR>1 {print $1}')"`).then(() =>
@@ -12,8 +20,15 @@ export const toggleOnScreenKeyboard = () =>
 export default function OnscreenKeyboard() {
   const icon = isOnScreenKeyboardVisible.as((v) => (v ? 'keyboard-off' : 'keyboard'));
   return (
-    <button onClicked={toggleOnScreenKeyboard} tooltipText="Toggle on-screen keyboard">
-      <Icon name={icon} />
-    </button>
+    <With value={hasTouchScreen}>
+      {(hasTouchScreen) => {
+        if (!hasTouchScreen) return null;
+        return (
+          <button onClicked={toggleOnScreenKeyboard} tooltipText="Toggle on-screen keyboard">
+            <Icon name={icon} />
+          </button>
+        );
+      }}
+    </With>
   );
 }
