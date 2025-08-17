7a63a4826a45dd46a70491a19782d2d2376f05ab
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-08-18 06:23:34 +1000

    Add NativeIcon component

--- modules/hyprland/ags/app.ts
+++ modules/hyprland/ags/app.ts
@@ -1,18 +1,20 @@
-import app from "ags/gtk4/app"
-import style from "./style.scss"
-import Bar from "./widget/Bar"
-import ControlCenter from "./widget/ControlCenter";
-import PasswordSearch from "./widget/PasswordSearch";
+import app from 'ags/gtk4/app';
+import style from './style.scss';
+import Bar from './widget/Bar';
+import ControlCenter from './widget/ControlCenter';
+import PasswordSearch from './widget/PasswordSearch';
+import { NativeIconProvider } from './components/NativeIcon';
 
 app.start({
   css: style,
   requestHandler: (req, res) => {
     console.log(req);
-    res('todo')
+    res('todo');
   },
   main() {
-    app.get_monitors().map(Bar)
-    app.get_monitors().map(ControlCenter)
-    PasswordSearch()
+    app.get_monitors().map(Bar);
+    app.get_monitors().map(ControlCenter);
+    PasswordSearch();
+    NativeIconProvider();
   },
-})
+});
