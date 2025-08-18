0413f31a548710fff8fc6b0f76aa05d25f55270c
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-08-19 05:18:22 +1000

    Add the rounded class

--- modules/hyprland/ags/widget/PasswordSearch.tsx
+++ modules/hyprland/ags/widget/PasswordSearch.tsx
@@ -113,7 +113,7 @@ export default function PasswordSearch() {
 
 export function PasswordSearchButton() {
   return (
-    <button onClicked={() => toggleWindow('password-search')}>
+    <button cssClasses={['rounded']} onClicked={() => toggleWindow('password-search')}>
       <Icon name="search" />
     </button>
   );
