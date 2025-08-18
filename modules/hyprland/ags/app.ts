381fec61d27ab3cf5508232702aee04c679d67b3
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-08-19 04:00:31 +1000

    Add toggle request command

--- modules/hyprland/ags/app.ts
+++ modules/hyprland/ags/app.ts
@@ -4,12 +4,36 @@ import Bar from './widget/Bar';
 import ControlCenter from './widget/ControlCenter';
 import PasswordSearch from './widget/PasswordSearch';
 import { NativeIconProvider } from './components/NativeIcon';
+import { toggleWindow, WindowName } from './utils/window';
+
+const HELP = `
+Usage
+$ ags <command>
+
+Commands
+toggle
+  - Toggle a window's visibility
+  - Usage: toggle <window_id>
+`.trim();
 
 app.start({
   css: style,
   requestHandler: (req, res) => {
-    console.log(req);
-    res('todo');
+    const [cmd, ...args] = req.split(' ');
+    switch (cmd) {
+      case 'toggle':
+        const windowId = args[0];
+        if (!windowId) {
+          res(HELP);
+          return;
+        }
+        toggleWindow(windowId as WindowName);
+        res(1);
+        return;
+      default:
+        res(HELP);
+        return;
+    }
   },
   main() {
     app.get_monitors().map(Bar);
