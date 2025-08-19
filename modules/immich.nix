3239865c0741dd7836de23bce341035ddbe18a1f
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-08-20 04:07:47 +1000

    Refactor immich.nix

--- modules/immich.nix
+++ modules/immich.nix
@@ -1,15 +1,27 @@
-{ env, pkgs, ... }: {
+{ env, pkgs, ... }:
+let
+  user = "immich";
+  group = "immich";
+in
+{
   services.immich = {
+    inherit user group;
     enable = true;
     openFirewall = true;
     accelerationDevices = null;
     mediaLocation = "/run/media/HDD_3/immich";
-    group = "immich";
   };
-  users.users.${env.user} = {
-    extraGroups = [ "immich" ];
-  };
-  environment.systemPackages = with pkgs; [
-    immich-cli
-  ];
+  users.users =
+    {
+      ${user} = {
+        extraGroups = [ "video" "render" ];
+      };
+      ${env.user} = {
+        extraGroups = [ group "video" "render" ];
+      };
+    };
+  environment.systemPackages = with pkgs;
+    [
+      immich-cli
+    ];
 }
