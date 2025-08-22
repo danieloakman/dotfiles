75d922640f9a6adcf1b1a2a2e502d382f67f004e
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-08-23 07:35:36 +1000

    Add Comparator fn

--- modules/hyprland/ags/utils/fn.ts
+++ modules/hyprland/ags/utils/fn.ts
@@ -6,3 +6,22 @@ export const once = <T extends (...args: any[]) => any>(fn: T) => {
 export const noop = () => {};
 
 export const iife = <T extends () => any>(fn: T): ReturnType<T> => fn();
+
+export interface Comparator<T, R extends number | boolean> {
+  (a: T, b: T): R;
+}
+
+/** Combines any number of comparators into a single comparator. Can be used for sorting or equality. */
+export function multiComparator<T, R extends number | boolean>(
+  ...comparators: Comparator<T, R>[]
+): Comparator<T, R> {
+  return (a: T, b: T) => {
+    let isBool = false;
+    for (const comparator of comparators) {
+      const result = comparator(a, b);
+      isBool = typeof result === 'boolean';
+      if (result) return result;
+    }
+    return (isBool ? false : 0) as R;
+  };
+}
