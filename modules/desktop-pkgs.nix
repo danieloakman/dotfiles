294e61c272a7b0ede955e52dc5ddce8fb39ba7f0
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-08-26 06:19:25 +1000

    Move Guake terminal to /gnome

--- modules/desktop-pkgs.nix
+++ modules/desktop-pkgs.nix
@@ -13,7 +13,6 @@
     '')
     # zed-editor # Trying this out as an alternative to vscode
     google-drive-ocamlfuse
-    inputs.guake.legacyPackages.${system}.guake
     # Was trying this out for a week or two. I think guake is just overall a better choice. `pass` autocomplete no longer worked for one. And making the terminal a paid subscription gave me the ick.
     # warp-terminal
     home-manager
