e5a1c136f2055d6626c0a5301cf5b99c119aed45
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-08-28 05:03:01 +1000

    Fix onScreenKeyboard not being visible

--- modules/hyprland/ags/widget/OnscreenKeyboard.tsx
+++ modules/hyprland/ags/widget/OnscreenKeyboard.tsx
@@ -2,15 +2,9 @@ import { execAsync } from 'ags/process';
 import Icon from '../components/Icon';
 import { createExternal, createState } from 'ags';
 import { noop } from '../utils/fn';
+import { hasTouchDevice } from '../utils/hyprland';
 
 const [isOnScreenKeyboardVisible, setIsOnScreenKeyboardVisible] = createState(false);
-
-export const hasTouchScreen = createExternal(false, (set) => {
-  execAsync('zsh -c "ls /sys/class/input/*/name | xargs cat"')
-    .then((res) => set(res.includes('touch')))
-    .catch(() => set(false));
-  return noop;
-});
 
 export const toggleOnScreenKeyboard = () =>
   execAsync(`zsh -c "kill -34 $(ps -C wvkbd-mobintl | awk 'NR>1 {print $1}')"`).then(() =>
@@ -19,8 +13,13 @@ export const toggleOnScreenKeyboard = () =>
 
 export default function OnscreenKeyboard() {
   const icon = isOnScreenKeyboardVisible.as((v) => (v ? 'keyboard-off' : 'keyboard'));
+  const visible = createExternal(false, (set) => {
+    hasTouchDevice().then(set);
+    return noop;
+  });
+
   return (
-    <box visible={hasTouchScreen}>
+    <box visible={visible}>
       <button onClicked={toggleOnScreenKeyboard} tooltipText="Toggle on-screen keyboard">
         <Icon name={icon} />
       </button>
