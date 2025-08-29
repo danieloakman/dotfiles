c670caaf060f2af2e826edcc0a264bfd6a241c88
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-08-30 06:35:34 +1000

    Enable nginx

--- modules/firefly.nix
+++ modules/firefly.nix
@@ -1,6 +1,7 @@
 { env, ... }: {
   services.firefly-iii = {
     enable = true;
+    enableNginx = true;
     settings = {
       APP_KEY_FILE = "/home/${env.user}/Sync/secrets/firefly-3/app-key.txt";
     };
