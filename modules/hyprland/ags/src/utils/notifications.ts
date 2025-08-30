4eba0aa8d0c5911e3a1e37a1c08efd25fa00743c
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-08-31 05:42:59 +1000

    notifications.ts changes

--- modules/hyprland/ags/src/utils/notifications.ts
+++ modules/hyprland/ags/src/utils/notifications.ts
@@ -12,34 +12,34 @@ export function setDontDisturb(value: boolean) {
   notifd.set_dont_disturb(value);
 }
 
-export function notify(message: string) {
+// export function notify(message: string) {
+// }
+
+export interface NotifyOptions<ActionKey extends string> {
+  icon?: NativeIcon;
+  actions?: Record<ActionKey, string>;
+  expire?: number;
+  urgency?: 'low' | 'normal' | 'critical';
 }
 
-// export interface NotifyOptions<ActionKey extends string> {
-//   icon?: NativeIcon;
-//   actions?: Record<ActionKey, string>;
-//   expire?: number;
-//   urgency?: 'low' | 'normal' | 'critical';
-// }
-
-// export async function notify<ActionKey extends string>(
-//   message: string,
-//   { icon, actions, expire = 5000, urgency = 'normal' }: NotifyOptions<ActionKey> = {},
-// ) {
-//   return execAsync(
-//     [
-//       'notify-send',
-//       message,
-//       `-t ${expire}`,
-//       `-u ${urgency}`,
-//       ...(icon ? ['-i', icon] : []),
-//       ...(actions ? Object.entries(actions).map(([key, text]) => `-A "${key}=${text}"`) : []),
-//     ].filter(Boolean),
-//   )
-//     .then((stdout) => {
-//       if (actions) return stdout;
-//     })
-//     .catch((error) => {
-//       console.error('Failed to send notification', error);
-//     });
-// }
+export async function notify<ActionKey extends string>(
+  message: string,
+  { icon, actions, expire = 5000, urgency = 'normal' }: NotifyOptions<ActionKey> = {},
+) {
+  return execAsync(
+    [
+      'notify-send',
+      message,
+      `-t ${expire}`,
+      `-u ${urgency}`,
+      ...(icon ? ['-i', icon] : []),
+      ...(actions ? Object.entries(actions).map(([key, text]) => `-A "${key}=${text}"`) : []),
+    ].filter(Boolean),
+  )
+    .then((stdout) => {
+      if (actions) return stdout;
+    })
+    .catch((error) => {
+      console.error('Failed to send notification', error);
+    });
+}
