12a07db82c01d95ca3c5575119a94c364b115ecc
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-09-02 12:54:00 +1000

    More work on getting darwin flake with home-manager to work

--- flakes/darwin/flake.nix
+++ flakes/darwin/flake.nix
@@ -3,160 +3,198 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin ={
+    nix-darwin = {
       url = "github:nix-darwin/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    home-manager= {
+    home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      nixpkgs = {
-        config.allowUnfree = true;
-        # The platform the configuration will be used on.
-        hostPlatform = "aarch64-darwin";
-      };
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, ... }:
+    let
+      configuration = { pkgs, ... }: {
+        nixpkgs = {
+          config.allowUnfree = true;
+          # The platform the configuration will be used on.
+          hostPlatform = "aarch64-darwin";
+        };
 
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment = {
-        systemPackages = with pkgs; [
-          nixfmt
-          nil
-          raycast
-        ];
-      };
+        # List packages installed in system profile. To search by name, run:
+        # $ nix-env -qaP | grep wget
+        environment = {
+          systemPackages = with pkgs; [
+            nixpkgs-fmt
+            nil
+            raycast
+            nodejs_24
+            bun
+            pnpm
+          ];
+        };
 
-      nix = {
-        # Necessary for using flakes on this system.
-        settings.experimental-features = "nix-command flakes";
-        # Use Determinate Nix:
-        enable = false;
-      };
+        nix = {
+          # Necessary for using flakes on this system.
+          settings.experimental-features = "nix-command flakes";
+          # Use Determinate Nix:
+          enable = false;
+        };
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
 
-      system = {
-        primaryUser = "daniel.brown";
+        system = {
+          # Set Git commit hash for darwin-version.
+          configurationRevision = self.rev or self.dirtyRev or null;
 
-        defaults = {
-          NSGlobalDomain = {
-            # Use F1, F2, etc. keys as standard function keys:
-            "com.apple.keyboard.fnState" = true;
-            AppleShowAllFiles = true;
-            AppleShowAllExtensions = true;
-            InitialKeyRepeat = 20;
-            KeyRepeat = 3;
-          };
-          controlcenter = {
-            BatteryShowPercentage = true;
-            Bluetooth = true;
-          };
-          dock = {
-            autohide = true;
-            autohide-delay = 0.1;
-            autohide-time-modifier = 0.2;
-            expose-animation-duration = 0.2;
-            tilesize = 36;
-            showhidden = true;
-            show-recents = false;
-            mru-spaces = false;
-          };
-          CustomUserPreferences = {
-            "com.apple.symbolichotkeys" = {
-              AppleSymbolicHotKeys = {
-                # Disable '^ + Space' for selecting the previous input source
-                "60" = { enabled = false; };
-                # Disable '^ + Option + Space' for selecting the next input source
-                "61" = { enabled = false; };
-                # Disable 'Cmd + Space' for Spotlight Search
-                "64" = { enabled = false; };
-                # Disable 'Cmd + Alt + Space' for Finder search window
-                "65" = { enabled = false; };
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          stateVersion = 6;
+
+          primaryUser = "daniel.brown";
+
+          defaults = {
+            NSGlobalDomain = {
+              # Use F1, F2, etc. keys as standard function keys:
+              "com.apple.keyboard.fnState" = true;
+              AppleShowAllFiles = true;
+              AppleShowAllExtensions = true;
+              InitialKeyRepeat = 20;
+              KeyRepeat = 3;
+            };
+            controlcenter = {
+              BatteryShowPercentage = true;
+              Bluetooth = true;
+            };
+            dock = {
+              autohide = true;
+              autohide-delay = 0.1;
+              autohide-time-modifier = 0.2;
+              expose-animation-duration = 0.2;
+              tilesize = 36;
+              showhidden = true;
+              show-recents = false;
+              mru-spaces = false;
+            };
+            CustomUserPreferences = {
+              "com.apple.symbolichotkeys" = {
+                AppleSymbolicHotKeys = {
+                  # Disable '^ + Space' for selecting the previous input source
+                  "60" = { enabled = false; };
+                  # Disable '^ + Option + Space' for selecting the next input source
+                  "61" = { enabled = false; };
+                  # Disable 'Cmd + Space' for Spotlight Search
+                  "64" = { enabled = false; };
+                  # Disable 'Cmd + Alt + Space' for Finder search window
+                  "65" = { enabled = false; };
+                };
+              };
+              "com.apple.TimeMachine".DoNotOfferNewDisksForBackup = true;
+              # Prevent Photos from opening automatically when devices are plugged in:
+              "com.apple.ImageCapture".disableHotPlug = true;
+              "com.apple.finder" = {
+                ShowExternalHardDrivesOnDesktop = true;
+                ShowHardDrivesOnDesktop = true;
+                ShowMountedServersOnDesktop = true;
+                ShowRemovableMediaOnDesktop = true;
+                _FXSortFoldersFirst = true;
+                # When performing a search, search the current folder by default
+                FXDefaultSearchScope = "SCcf";
+              };
+              "com.apple.desktopservices" = {
+                # Avoid creating .DS_Store files on network or USB volumes
+                DSDontWriteNetworkStores = true;
+                DSDontWriteUSBStores = true;
               };
             };
-            "com.apple.TimeMachine".DoNotOfferNewDisksForBackup = true;
-            # Prevent Photos from opening automatically when devices are plugged in:
-            "com.apple.ImageCapture".disableHotPlug = true;
-            "com.apple.finder" = {
-              ShowExternalHardDrivesOnDesktop = true;
-              ShowHardDrivesOnDesktop = true;
-              ShowMountedServersOnDesktop = true;
-              ShowRemovableMediaOnDesktop = true;
-              _FXSortFoldersFirst = true;
-              # When performing a search, search the current folder by default
-              FXDefaultSearchScope = "SCcf";
-            };
-            "com.apple.desktopservices" = {
-              # Avoid creating .DS_Store files on network or USB volumes
-              DSDontWriteNetworkStores = true;
-              DSDontWriteUSBStores = true;
-            };
           };
+
+          activationScripts.postActivation.text = ''
+            # Check if any settings have been changed and apply them without a logout/login cycle:
+            sudo -u daniel.brown /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+          '';
         };
-      };
 
-      homebrew = {
-        enable = true;
-
-        brews = [
-          "gh"
-          "git"
-          "eza"
-          "bat"
-          "lazygit"
-          "pass"
-          "pass-otp"
-          "gemini-cli"
-        ];
-
-        casks = [
-          "cursor"
-          "vivaldi"
-          "visual-studio-code"
-          "warp"
-        ];
-      };
-
-      programs = {
-        zsh = {
+        homebrew = {
           enable = true;
-          enableAutosuggestions = true;
-          enableFastSyntaxHighlighting = true;
-          variables = {
-            PASSWORD_STORE_DIR = "$HOME/repos/personal/pwd-store";
-          };
-          # ohMyZsh = {
-          #   enable = true;
-          #   theme = "robbyrussell";
-          # };
-          # plugins = [
-          #   "git"
-          # ];
-        };
-      };
 
-      system.activationScripts.postActivation.text = ''
-        # Check if any settings have been changed and apply them without a logout/login cycle:
-        sudo -u daniel.brown /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
-      '';
+          brews = [
+            "gh"
+            "git"
+            "eza"
+            "bat"
+            "lazygit"
+            "pass"
+            "pass-otp"
+            "gemini-cli"
+            "fastfetch"
+            "pinentry-mac"
+            "btop"
+            {
+              name = "syncthing";
+              restart_service = "changed";
+            }
+          ];
+
+          casks = [
+            "cursor"
+            "vivaldi"
+            "visual-studio-code"
+            "warp"
+          ];
+        };
+
+        programs = {
+          zsh = {
+            enable = true;
+            enableAutosuggestions = true;
+            enableFastSyntaxHighlighting = true;
+            variables = {
+              PASSWORD_STORE_DIR = "$HOME/repos/personal/pwd-store";
+            };
+            # ohMyZsh = {
+            #   enable = true;
+            #   theme = "robbyrussell";
+            # };
+            # plugins = [
+            #   "git"
+            # ];
+          };
+        };
+
+        networking.hostName = "boethiah";
+      };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#boethiah
+      darwinConfigurations =
+        let
+          config = {
+            system = "aarch64-darwin";
+            modules = [
+              configuration
+              # home-manager.darwinModules.home-manager
+              # {
+              #   home-manager = {
+              #     useGlobalPkgs = true;
+              #     useUserPackages = true;
+              #     extraSpecialArgs = { inherit inputs; };
+              #     users."daniel.brown" = ./modules/home.nix;
+              #   };
+              # }
+            ];
+          };
+        in
+        {
+          # TODO: just use one of these host names. For some reason changing the host name on mac isn't always working.
+          "boethiah" = nix-darwin.lib.darwinSystem {
+            inherit (config) modules;
+          };
+          "MY797HJWD7" = nix-darwin.lib.darwinSystem {
+            inherit (config) modules;
+          };
+        };
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#boethiah
-    darwinConfigurations."boethiah" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-  };
 }
---
+++ flakes/darwin/modules/home.nix
@@ -0,0 +1,53 @@
+_: {
+  # Turns out we need this in home-manager as well. It's not enough to just have it in the system configuration:
+  nixpkgs.config.allowUnfree = true;
+
+  home = {
+    username = "daniel.brown";
+    homeDirectory = "/Users/daniel.brown";
+    stateVersion = "25.05";
+
+    file.".gitconfig".text = ''
+      [user]
+        name = Daniel (Oakman) Brown
+        email = 42539848+danieloakman@users.noreply.github.com
+        signingkey = 8FB975523F3FEB6113801C04368C0A3C6913D768
+      [credential]
+        helper = cache --timeout 604800
+      [commit]
+        gpgsign = true
+      [init]
+        defaultBranch = main
+      [gpg]
+        program = gpg
+      [pull]
+        ff = true
+      [core]
+        editor = nano
+      [http]
+        postBuffer = 524288000
+      [gpg "ssh"]
+        allowedSignersFile = ~/.config/git/allowed_signers
+      [credential "https://github.com"]
+        helper = 
+        helper = !/opt/homebrew/bin/gh auth git-credential
+      [credential "https://gist.github.com"]
+        helper = 
+        helper = !/opt/homebrew/bin/gh auth git-credential
+    '';
+  };
+
+  # Let Home Manager install and manage itself.
+  programs = {
+    home-manager.enable = true;
+  };
+
+  services = {
+    gpg-agent = {
+      enable = true;
+      defaultCacheTtl = 604800;
+      enableSshSupport = true;
+      pinentry.program = "/opt/homebrew/bin/pinentry-mac";
+    };
+  };
+}
