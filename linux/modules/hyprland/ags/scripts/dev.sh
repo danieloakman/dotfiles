77353df882d57cb85245e6a9605b50366f6a47dc
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-09-03 05:05:59 +1000

    Fix ags scripts

--- linux/modules/hyprland/ags/scripts/dev.sh
+++ linux/modules/hyprland/ags/scripts/dev.sh
@@ -1,4 +1,4 @@
 #! bash
 
-AGS_DIR=$DOTFILES_DIR/flakes/linux/modules/hyprland/ags
+AGS_DIR=$DOTFILES_DIR/linux/modules/hyprland/ags
 find $AGS_DIR -type f \( -name "*.tsx" -o -name "*.scss" \) | entr -r ags run $AGS_DIR/app.tsx
