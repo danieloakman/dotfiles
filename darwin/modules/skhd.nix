757f9144e37a0b9ec31b19fce9af086894b622e0
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-09-03 15:40:25 +1000

    Fix skhd

--- darwin/modules/skhd.nix
+++ darwin/modules/skhd.nix
@@ -1,16 +1,20 @@
 { env, ... }: {
-  home-manager.users.${env.user}.programs.skhd = {
-    enable = true;
-    config = ''
-      0x3B + left : aerospace focus left
-      0x3B + right : aerospace focus right
-      0x3B + up : aerospace focus up
-      0x3B + down : aerospace focus down
+  home-manager.users.${env.user} = {
+    services.skhd = {
+      enable = true;
+      outLogFile = "/tmp/skhd.log";
+      errorLogFile = "/tmp/skhd-error.log";
+      config = ''
+        0x3B + left : aerospace focus left
+        0x3B + right : aerospace focus right
+        0x3B + up : aerospace focus up
+        0x3B + down : aerospace focus down
 
-      0x3B + shift + left : aerospace move left
-      0x3B + shift + right : aerospace move right
-      0x3B + shift + up : aerospace move up
-      0x3B + shift + down : aerospace move down
-    '';
+        0x3B + shift + left : aerospace move left
+        0x3B + shift + right : aerospace move right
+        0x3B + shift + up : aerospace move up
+        0x3B + shift + down : aerospace move down
+      '';
+    };
   };
 }
