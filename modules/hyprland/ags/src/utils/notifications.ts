bc6295b60ec7afbfcb50a7da4b5896e2107cb17a
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-09-09 04:10:15 +1000

    More swaync ags changes

--- modules/hyprland/ags/src/utils/notifications.ts
+++ modules/hyprland/ags/src/utils/notifications.ts
@@ -1,9 +1,13 @@
 import { NativeIcon } from '@/types/icons';
-import { createState } from 'ags';
-import { execAsync } from 'ags/process';
+import { createBinding, createExternal, createState } from 'ags';
+import { execAsync, createSubprocess } from 'ags/process';
+import { safeJSONParse } from './object';
+import { raise } from './fn';
+import { createExternalState } from './ags';
+
+// ***ASTAL NOTIFD IMPLEMENTATION***
 // import Notifd from 'gi://AstalNotifd';
 // const notifd = Notifd.get_default();
-
 const [astalNotifications] = createState<import('gi://AstalNotifd').default.Notification[]>([]);
 export { astalNotifications };
 // export const notifications = createBinding(notifd, 'notifications').as((notifications) => {
@@ -14,18 +18,39 @@ export { astalNotifications };
 //     if (results.has(key)) notification.dismiss();
 //     else results.set(key, notification);
 //   }
-
 //   return Array.from(results.values()).sort((a, b) => b.get_time() - a.get_time());
 // });
-
 // export const dontDisturb = createBinding(notifd, 'dont_disturb');
+
+export interface SwayncStatus {
+  /** Number of notifications. */
+  count: number;
+  /** Whether Do Not Disturb is enabled. */
+  dnd: boolean;
+  /** Whether the notification panel is visible. */
+  visible: boolean;
+  /** Whether notifications are inhibited, i.e. prevented from being displayed. */
+  inhibited: boolean;
+}
+
+export const swayncStatus = createSubprocess('', 'zsh -c "swaync-client -s"').as((stdout) => {
+  const {
+    count = 0,
+    dnd = false,
+    visible = false,
+    inhibited = false,
+  } = safeJSONParse<SwayncStatus>(stdout) ?? {};
+  return { count, dnd, visible, inhibited };
+});
 
 export function setDontDisturb(value: boolean) {
   // notifd.set_dont_disturb(value);
+  execAsync(`swaync-client ${value ? '-dn' : '-df'}`);
 }
 
 export function clearNotifications() {
   // for (const notification of notifd.get_notifications()) notification.dismiss();
+  execAsync('swaync-client -C').catch((err) => console.error('Failed to clear notifications', err));
 }
 
 export interface NotifyOptions<ActionKey extends string> {
--- modules/hyprland/ags/src/widget/Swaync.tsx
+++ modules/hyprland/ags/src/widget/Swaync.tsx
@@ -1,11 +1,20 @@
 import { Icon } from '@/components/Icon';
 import { classes } from '../utils/styles';
 import { execAsync } from 'ags/process';
+import { swayncStatus } from '@/utils/notifications';
 
 export default function Swaync() {
   return (
-    <button name="SwayNC" cssClasses={classes('rounded')} onClicked={() => execAsync('swaync-client -t')}>
-      <Icon name="bell" />
+    <button
+      name="SwayNC"
+      cssClasses={classes('rounded')}
+      onClicked={() => execAsync('swaync-client -t')}
+    >
+      <Icon
+        name={swayncStatus(({ dnd, count }) =>
+          dnd ? 'bell-off' : count > 0 ? 'bell-dot' : 'bell',
+        )}
+      />
     </button>
   );
 }
