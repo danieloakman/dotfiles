1bace4941b9f895a6be0926814a83325e8cd3866
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-09-08 13:34:29 +1000

    alt+n binds now use a custom script

--- modules/skhd.nix
+++ modules/skhd.nix
@@ -1,41 +1,52 @@
-{ lib, pkgs, ... }: {
+{ env, lib, pkgs, ... }:
+let
+  openApp = pkgs.writeShellScriptBin "open-app" ''
+    APP_NAME="$1"
+
+    # Check if app is running
+    if pgrep -x "$APP_NAME" > /dev/null; then
+        # Check if app is frontmost
+        FRONTMOST=$(osascript -e 'tell application "System Events" to get name of first process whose frontmost is true')
+
+        if [ "$FRONTMOST" = "$APP_NAME" ]; then
+            # App is focused, hide it
+            osascript -e "tell application \"System Events\" to set visible of process \"$APP_NAME\" to false"
+        else
+            # App is running but not focused, bring to front
+            osascript -e "tell application \"$APP_NAME\" to activate"
+        fi
+    else
+        # App is not running, launch it
+        open -a "$APP_NAME"
+    fi
+  '';
+in
+{
   config = lib.mkIf pkgs.stdenv.isDarwin {
-    services.skhd = {
-      enable = true;
-      skhdConfig = ''
-        alt - 1 : open -a "Vivaldi"
-        alt - 2 : open -a "Cursor"
-        alt - 3 : open -a "Microsoft Teams"
-        alt - 4 : open -a "Roam"
-        alt - 5 : open -a "zoom.us"
-      '';
-    };
-
-    # home-manager.users.${env.user} = {
-    #   services.skhd = {
-    #     enable = true;
-    #     outLogFile = "/tmp/skhd.log";
-    #     errorLogFile = "/tmp/skhd-error.log";
-    #     config = ''
-    #       alt - 1 : open -a "Vivaldi"
-    #       alt - 2 : open -a "Cursor"
-    #       alt - 3 : open -a "Microsoft Teams"
-    #       alt - 4 : open -a "Roam"
-    #       alt - 5 : open -a "Zoom"
-
-    #       # 0x3B is supposed to be the fn key.
-    #       # Doesn't work
-    #       # 0x3B + left : aerospace focus left
-    #       # 0x3B + right : aerospace focus right
-    #       # 0x3B + up : aerospace focus up
-    #       # 0x3B + down : aerospace focus down
-
-    #       # 0x3B + shift + left : aerospace move left
-    #       # 0x3B + shift + right : aerospace move right
-    #       # 0x3B + shift + up : aerospace move up
-    #       # 0x3B + shift + down : aerospace move down
-    #     '';
-    #   };
+    # services.skhd = {
+    #   enable = true;
+    #   skhdConfig = ''
+    #     alt - 1 : open -a "Vivaldi"
+    #     alt - 2 : open -a "Cursor"
+    #     alt - 3 : ${openApp}/bin/open-app "Microsoft Teams"
+    #     alt - 4 : ${openApp}/bin/open-app "Roam"
+    #     alt - 5 : ${openApp}/bin/open-app "zoom.us"
+    #   '';
     # };
+
+    home-manager.users.${env.user} = {
+      services.skhd = {
+        enable = true;
+        outLogFile = "/tmp/skhd.log";
+        errorLogFile = "/tmp/skhd-error.log";
+        config = ''
+          alt - 1 : ${openApp}/bin/open-app "Vivaldi"
+          alt - 2 : ${openApp}/bin/open-app "Cursor"
+          alt - 3 : ${openApp}/bin/open-app "Microsoft Teams"
+          alt - 4 : ${openApp}/bin/open-app "Roam"
+          alt - 5 : ${openApp}/bin/open-app "zoom.us"
+        '';
+      };
+    };
   };
 }
