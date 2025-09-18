f06d45dfb8d08b6add3fa2aecb4a75fa42ab30ed
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-09-19 04:28:55 +1000

    Add a comment

--- modules/pia.nix
+++ modules/pia.nix
@@ -4,6 +4,7 @@
   ];
   # Provides `pia` command.
   # Currently doesn't work, see issue: https://github.com/Fuwn/pia.nix/issues/2
+  # Ended up just populating networkmanager vpn connections with the script from this repo: https://github.com/ThePowerTool/PIA-NetworkManager-GUI-Support
   services.pia = {
     enable = true;
     authUserPassFile = config.sops.secrets.pia_credentials.path;
