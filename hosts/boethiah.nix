7b42985cad2f09f036504acfc8cba35c69be2846
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-10-13 16:36:30 +1100

    Revert "Add filezilla to boethiah"
    This reverts commit ca7045bd307b02fee6b092b8766e35f9bf96cd31.

--- hosts/boethiah.nix
+++ hosts/boethiah.nix
@@ -97,7 +97,6 @@
       pnpm-shell-completion
       pet # CLI tool for keeping a list of commands and executing them later
       cursor-cli
-      filezilla
 
       # Golang & related tools:
       go
