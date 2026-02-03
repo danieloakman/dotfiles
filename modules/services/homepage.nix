a3007f08a8af8ce3294f500c3b9d4d1f27c69dd0
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2026-02-03 12:26:42 +1100

    Add adguard to homepage

--- modules/services/homepage.nix
+++ modules/services/homepage.nix
@@ -43,6 +43,12 @@ in
       services = [{
         "Services" = [
           {
+            "Adguard Home" = {
+              description = "Adblocking";
+              href = "http://100.116.141.37";
+            };
+          }
+          {
             "Cockpit" = {
               description = "System management";
               # href = "http://mara-cockpit.dinosaur-crocodile.ts.net";
