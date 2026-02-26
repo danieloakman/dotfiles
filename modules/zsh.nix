d946ccf7f0cab5ea82fe86629221d24f44a3b733
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2026-02-27 06:21:36 +1100

    Add to zshrc a possible fix

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,4 +1,4 @@
-{ env, ... }: {
+{ env, config, ... }: {
   home-manager.users.${env.user} = {
     programs = {
       zsh = {
@@ -7,14 +7,21 @@
         enableCompletion = true;
         syntaxHighlighting.enable = true;
         initContent = ''
+          # This enables included pass extensions in the password store itself (/.extension dir). For some reason this has to go here since putting it in the `sessionVariables` env var doesn't work.
+          export PASSWORD_STORE_ENABLE_EXTENSIONS="true"
+          export PASSWORD_STORE_DIR="$HOME/repos/personal/pwd-store"
+
+          # Attempt to unlock the password store using the DANO_PWD file.
+          DANO_PWD=${config.sops.secrets.dano_pwd.path}
+          if [ -f "$DANO_PWD" ]; then
+            cat "$DANO_PWD" | gpg --decrypt $(ls $PASSWORD_STORE_DIR/*.gpg | head -n 1) > /dev/null
+          fi
+
           # Put at the bottom of ".zshrc":
           if [ -f "$HOME/repos/personal/dotfiles/files/home/.shell_scripts/.main_shell" ]; then
             source "$HOME/repos/personal/dotfiles/files/home/.shell_scripts/.main_shell"
           fi
 
-          # This enables included pass extensions in the password store itself (/.extension dir). For some reason this has to go here since putting it in the `sessionVariables` env var doesn't work.
-          export PASSWORD_STORE_ENABLE_EXTENSIONS="true"
-          export PASSWORD_STORE_DIR="$HOME/repos/personal/pwd-store"
         '';
         envExtra = ''
           fpath=(${env.home}/.dgranted/zsh_autocomplete/assume/ $fpath)
