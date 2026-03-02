94b9f92fd452803af81db90f246840477e93bd18
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2026-03-02 16:56:39 +1100

    cleanup

--- modules/scripts/bun/default.nix
+++ modules/scripts/bun/default.nix
@@ -1,43 +1,41 @@
-{ bun2nix, system, pkgs, lib, config, ... }:
+{ bun2nix, system, pkgs, lib, ... }:
 let
   # See: https://nix-community.github.io/bun2nix/building-packages
   fetchBunDeps = bun2nix.packages.${system}.default.fetchBunDeps;
   hook = bun2nix.packages.${system}.default.hook;
-
-  builtDerivation = pkgs.stdenv.mkDerivation {
-    pname = "bun-scripts";
-    version = "0.1.0";
-
-    src = ./.;
-
-    nativeBuildInputs = with pkgs; [
-      hook
-      bun
-      makeWrapper
-    ];
-
-    bunDeps = fetchBunDeps {
-      bunNix = ./bun.nix;
-    };
-
-    buildPhase = ''
-      bun run build
-    '';
-
-    installPhase = ''
-      mkdir -p $out/dist $out/bin
-      cp -R ./dist/* $out/dist/
-      for f in $out/dist/*.js; do
-        name=$(basename "$f" .js)
-        makeWrapper ${lib.getExe pkgs.bun} $out/bin/$name \
-          --add-flags "run" --add-flags "$f" \
-          --argv0 "$name"
-      done
-    '';
-  };
 in
 {
   environment.systemPackages = [
-    builtDerivation
+    (pkgs.stdenv.mkDerivation {
+      pname = "bun-scripts";
+      version = "0.1.0";
+
+      src = ./.;
+
+      nativeBuildInputs = with pkgs; [
+        hook
+        bun
+        makeWrapper
+      ];
+
+      bunDeps = fetchBunDeps {
+        bunNix = ./bun.nix;
+      };
+
+      buildPhase = ''
+        bun run build
+      '';
+
+      installPhase = ''
+        mkdir -p $out/dist $out/bin
+        cp -R ./dist/* $out/dist/
+        for f in $out/dist/*.js; do
+          name=$(basename "$f" .js)
+          makeWrapper ${lib.getExe pkgs.bun} $out/bin/$name \
+            --add-flags "run" --add-flags "$f" \
+            --argv0 "$name"
+        done
+      '';
+    })
   ];
 }
