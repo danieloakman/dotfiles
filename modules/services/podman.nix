bea3030f150b6ba12abe9b1ae6151ab92aa9ce19
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2026-04-20 11:45:28 +1000

    Add krunkit

--- modules/services/podman.nix
+++ modules/services/podman.nix
@@ -1,4 +1,10 @@
-{ env, config, lib, ... }:
+{
+  env,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.my.services.podman;
 in
@@ -6,15 +12,22 @@ in
   options.my.services.podman = {
     enable = lib.mkEnableOption "Enable and install Podman and related packages/services.";
   };
-  config = lib.mkIf cfg.enable (env.selectPlatform {
-    darwin.homebrew = {
-      brews = [
-        "podman"
-        "podman-tui"
-        "podman-compose"
-      ];
-      casks = [ "podman-desktop" ];
-    };
-    # Linux support is not yet implemented
-  });
+  config = lib.mkIf cfg.enable (
+    env.selectPlatform {
+      darwin = {
+        environment.systemPackages = with pkgs; [
+          krunkit # Podman desktop requires this
+        ];
+        homebrew = {
+          brews = [
+            "podman"
+            "podman-tui"
+            "podman-compose"
+          ];
+          casks = [ "podman-desktop" ];
+        };
+      };
+      # Linux support is not yet implemented
+    }
+  );
 }
