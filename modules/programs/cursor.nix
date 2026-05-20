0422874b2ad87563ec7b7aafde7671713f8a3d09
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2026-05-20 14:23:06 +1000

    Fix cursor-cli

--- modules/programs/cursor.nix
+++ modules/programs/cursor.nix
@@ -17,20 +17,24 @@ in
 
   config = lib.mkIf cfg.enable (
     env.selectPlatform {
+      any = {
+        environment.systemPackages = [
+          cursor-cli
+        ];
+      };
       linux = {
         environment.systemPackages = [
           pkgs.code-cursor
-          cursor-cli
+          # TODO: see if this script is still necessary on linux. Reason its here is because logs would be spewed out every time `cursor` was used from cli
           (pkgs.writeShellScriptBin "open-cursor" ''
             # Opens the cursor editor in the current directory
               ${lib.getExe pkgs.code-cursor} . &> /tmp/cursor.log &
           '')
         ];
       };
-      # TODO: migrate cursor config from boethiah to here.
       darwin.homebrew.casks = [
         "cursor"
-        "cursor-cli"
+        # "cursor-cli" # boethiah can't install and use this for some reason.
       ];
     }
   );
