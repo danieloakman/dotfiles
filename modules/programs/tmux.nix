67f146947073e93e3b0debe0fcd788243aa09d3a
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2026-06-06 17:36:23 +1000

    use hm module for tmux

--- modules/programs/tmux.nix
+++ modules/programs/tmux.nix
@@ -5,21 +5,17 @@ in
 {
   options.my.programs.tmux.enable = lib.mkEnableOption "Enable and configure tmux.";
 
-  config = lib.mkIf cfg.enable (env.selectPlatform {
-    any.programs.tmux = {
-      enable = true;
-
-      extraConfig = ''
-        # Custom keybinds for splitting
-        bind-key -n C-M-= split-window -h
-        bind-key -n C-M-- split-window -v
-      '';
+  config = lib.mkIf cfg.enable {
+    home-manager.users.${env.user} = {
+      programs.tmux = {
+        enable = true;
+        keyMode = "vi";
+        extraConfig = ''
+          # Custom keybinds for splitting
+          bind-key -n C-M-= split-window -h
+          bind-key -n C-M-- split-window -v
+        '';
+      };
     };
-    linux.programs.tmux = {
-      keyMode = "vi";
-    };
-    darwin.programs.tmux = {
-      enableVim = true;
-    };
-  });
+  };
 }
