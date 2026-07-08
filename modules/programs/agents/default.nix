0c3b98320b8d2ff3429a7fc2f9083d853e3f705a
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2026-07-08 12:16:30 +1000

    Revert "Move agent context so claude root context can be non-read-only again"
    This reverts commit 06ff63fe40a06bf13edbd55c5c3be035949c73ff.

--- modules/programs/agents/default.nix
+++ modules/programs/agents/default.nix
@@ -52,10 +52,6 @@ in
   config = lib.mkIf cfg.enable {
     home-manager.users.${env.user} = {
       home.file = {
-        ".claude/my-context.md" = {
-          text = cfg.rootContext;
-          force = false;
-        };
         ".agents/AGENTS.md".text = cfg.rootContext;
         ".config/agents/AGENTS.md".text = cfg.rootContext;
         # Cursor uses a list of rules defined in the .cursor/rules directory. So for now we're just adding a global rule. Cursor may not even support reading rules from files like this... Maybe remove in the future.
@@ -91,9 +87,7 @@ in
         cfg.skillDirs;
       programs = {
         claude-code = {
-          context = ''
-            @my-context.md
-          '';
+          context = cfg.rootContext;
           # At the moment, cursor supports finding skills in the .claude/skills directory, as do many other agents.
           # If for some reason in the future they don't we could probably just run an activate block that symlinks from claude/skills to whatever other directory we want to use also.
           inherit (cfg) skills;
