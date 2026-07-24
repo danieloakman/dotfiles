630e7ed67009b4c81e05c1c4266c2e429cc04b57
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2026-07-24 18:27:56 +1000

    Add ttyd web terminal service module (#31)
    * Add ttyd web terminal service for phone access via Tailscale.
    
    * Leave ttyd module available but disabled on mara.

---
+++ modules/services/ttyd.linux.nix
@@ -0,0 +1,42 @@
+{ pkgs, lib, config, env, ... }:
+let
+  cfg = config.my.services.ttyd;
+  inherit (cfg) port;
+  portStr = toString port;
+in
+{
+  options.my.services.ttyd = {
+    enable = lib.mkEnableOption "ttyd web terminal";
+    port = lib.mkOption {
+      type = lib.types.port;
+      default = 7681;
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.ttyd = {
+      enable = true;
+      inherit port;
+      interface = "127.0.0.1";
+      user = env.user;
+      writeable = true;
+      entrypoint = [ (lib.getExe pkgs.zsh) ];
+    };
+
+    systemd.services.ttyd.path = [ pkgs.zsh ];
+
+    environment.systemPackages = with pkgs; [
+      (writeShellScriptBin "tailscale-svc-ttyd-up" ''
+        tailscale serve --service=svc:ttyd --https=443 http://127.0.0.1:${portStr}
+      '')
+      (writeShellScriptBin "tailscale-svc-ttyd-down" ''
+        tailscale serve clear svc:ttyd
+      '')
+    ];
+
+    my.services.homepage.services."ttyd" = {
+      description = "Web terminal";
+      href = "https://ttyd.dinosaur-crocodile.ts.net";
+    };
+  };
+}
